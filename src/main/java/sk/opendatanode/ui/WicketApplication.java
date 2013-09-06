77b7670328edaaf422562817e7f0965d72a600c3
Peter Hanecak <hanecak@opendata.sk>
2013-09-06 14:43:43 +0200

    reverted totally embarrassing typo which slipped into previous commit

--- src/main/java/sk/opendatanode/ui/WicketApplication.java
+++ src/main/java/sk/opendatanode/ui/WicketApplication.java
@@ -23,7 +23,7 @@ import org.apache.wicket.Page;
 import org.apache.wicket.protocol.http.WebApplication;
 
 import sk.opendatanode.ui.results.ResultDocumentPage;
-pre pripady, ked pracujeme na open source projekte
+
 /**
  * Application object for your web application. If you want to run this application without deploying, run the Start class.
  * 
